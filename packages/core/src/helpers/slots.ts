d97a313486cf06291d52cdef4b573c8b58c4f22b
Sami Jaber <jabersami@gmail.com>
2023-10-30 14:17:47 -0300

    yee

--- packages/core/src/helpers/slots.ts
+++ packages/core/src/helpers/slots.ts
@@ -1,11 +1,20 @@
 import { types } from '@babel/core';
+import { createMitosisNode, MitosisNode, Plugin } from '..';
 import { babelTransformExpression } from './babel-transform';
+import { traverseNodes } from './traverse-nodes';
 
 const SLOT_PREFIX = 'slot';
 export type SlotMapper = (slotName: string) => string;
 
-export const isSlotProperty = (key: string, slotPrefix: string = SLOT_PREFIX): boolean =>
-  key.startsWith(slotPrefix);
+export const isSlotProperty = (key: string, slotPrefix: string = SLOT_PREFIX): boolean => {
+  if (key.startsWith(slotPrefix)) {
+    const firstChar = key[slotPrefix.length];
+    // matches `slotTesting`, but not `slottesting`
+    return firstChar === firstChar.toUpperCase();
+  }
+
+  return false;
+};
 
 export const stripSlotPrefix = (key: string, slotPrefix: string = SLOT_PREFIX): string =>
   isSlotProperty(key, slotPrefix) ? key.substring(slotPrefix.length) : key;
@@ -21,3 +30,29 @@ export function replaceSlotsInString(code: string, mapper: SlotMapper) {
     },
   });
 }
+
+// this should run at the parser level. slot prop should immediately become a child node.
+export const moveSlotsPlugin: Plugin = () => ({
+  json: {
+    pre: (json) => {
+      traverseNodes(json, (node) => {
+        Object.keys(node.bindings)
+          .filter((key) => isSlotProperty(key))
+          .forEach((key) => {
+            const binding = node.bindings[key]!;
+            const slotChild: MitosisNode = createMitosisNode({
+              '@type': '@builder.io/mitosis/node',
+              name: 'Slot',
+              properties: {
+                name: binding.code,
+              },
+              children: [],
+            });
+            node.children.push(slotChild);
+            delete node.bindings[key];
+          });
+      });
+      return json;
+    },
+  },
+});
